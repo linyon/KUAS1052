638f973ed59967401a485e385b08229edb45cb2c
YinChang0626 <yinchang0626@gmail.com>
2017-05-19 08:36:09 +0800

    範例4

---
+++ 範例程式/4.HTML_CSS_JS/Models/Repository/StationRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YC.Repository
+{
+    public class StationRepository
+    {
+        private string _connectionString= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\WaterDB.mdf;Integrated Security=True";
+ 
+
+
+        public void Create(List<Models.Station> stations)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection();
+            connection.ConnectionString = _connectionString;
+            connection.Open();
+
+            foreach (var station in stations)
+            {
+                var command = new System.Data.SqlClient.SqlCommand("", connection);
+                command.CommandText = string.Format(@"
+INSERT        INTO    Station(ID, LocationAddress, ObservatoryName, LocationByTWD67, CreateTime)
+VALUES          (N'{0}',N'{1}',N'{2}',N'{3}',N'{4}')
+", station.ID, station.LocationAddress, station.ObservatoryName.Replace("'","''"), station.LocationByTWD67, station.CreateTime.ToString("yyyy/MM/dd"));
+
+                command.ExecuteNonQuery();
+            }
+
+
+
+            connection.Close();
+        }
+
+
+        public List<Models.Station> FindAllStations()
+        {
+            var result = new List<Models.Station>();
+            var connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+            connection.Open();
+            var command = new System.Data.SqlClient.SqlCommand("", connection);
+            command.CommandText = @"
+Select * from Station";
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Models.Station item = new Models.Station();
+                item.ID = reader["ID"].ToString();
+                item.LocationAddress = reader["LocationAddress"].ToString();
+                item.ObservatoryName = reader["ObservatoryName"].ToString();
+                item.LocationByTWD67 = reader["LocationByTWD67"].ToString();
+                item.CreateTime = DateTime.Parse(reader["CreateTime"].ToString());
+                result.Add(item);
+            }
+            connection.Close();
+
+
+            return result;
+        }
+
+
+
+    }
+}
